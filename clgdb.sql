64430f75c01b28deba115c43fa4c729126018832
Shohrat Singh <contactshohrat06@gmail.com>
2023-11-04 09:46:20 +0530

    Updated

--- clgdb.sql
+++ clgdb.sql
@@ -85,3 +85,89 @@ values
 truncate table BBA_student;
 
 select * from BBA_student;
+
+-----------------------------------------------------------------------------------------------------
+-----------------------------------------------------------------------------------------------------
+
+CREATE TABLE BEd_student(
+id INT PRIMARY KEY,
+std_name VARCHAR(30) NOT NULL,
+roll_no INT,
+std_address VARCHAR(50),
+phone_no BIGINT(10) NOT NULL,
+alt_phone_no BIGINT(10) DEFAULT NULL,
+semester INT DEFAULT 1
+);
+
+select * from BEd_student;
+
+desc BEd_student;
+
+drop table BEd_student;
+
+INSERT INTO BEd_student (id, std_name, roll_no, std_address, phone_no, alt_phone_no, semester)
+VALUES
+( 101, "Aanya Agarwal", 01 , " Gurugram" , 9876543401 , 9876543402 , 1 ),
+( 102, "Aarav Bajaj", 02 , "Varanasi" , 9876543403 , 9876543404 , 1 ),
+( 103, "Aarti Chandra", 03 , "Delhi" , 9876543405 , 9876543406 , 1 ),
+( 104, "Abhishek Das", 04 , "Gurugram" , 9876543407 , 9876543408 , 1 ),
+( 105, "Aditi Gupta", 05 , "Jaipur" , 9876543409 , 9876543410 , 1 ),
+( 106, "Aditya Jain", 06 , "Pataudi" , 9876543411 , 9876543412 , 1 ),
+( 107, "Agastya Kapoor ", 07 , "Lucknow", 9876543413 , 9876543414 , 1 ),
+( 108, "Ahana Malhotra", 08 , "Chandigarh" , 9876543415 , 9876543416 , 1 ),
+( 109, "Aisha Mehta", 09 , "Jammu" , 9876543417 , 9876543418 , 1 ),
+( 110, "Akhil Patel", 10 , "Amritsar" , 9876543419 , 9876543420 , 1 ),
+( 111, "Akshara Sharma", 11 , "Agra" , 9876543421 , null , 1 ),
+( 112, "Aman Singh", 12 , "Dehradun" , 9876543423 , 9876543424 , 1 ),
+( 113, "Anamika Thakur", 13 , "Shimla" , 9876543425 , 9876543426 , 1 ),
+( 114, "Ananya Verma", 14 , "Kanpur" , 9876543427 , 9876543428 , 1 ),
+( 115, "Anish Yadav", 15 , "Bhopal" , 9876543429 , 9876543430 , 1 ),
+( 116, "Anjali Joshi", 16 , "Kanpur" , 9876543431 , null , 1 ),
+( 117, "Ankit Kumar", 17 , "Indore" , 9876543433 , 9876543434 , 1 ),
+( 118, "Anurag Mishra", 18 , "Udaipur" , 9876543435 , null , 1 ),
+( 119, "Aparajita Nair", 19 , "Allahabad" , 9876543437 , 9876543438 , 1 ),
+( 120, "Arjun Oberoi", 20 , "Allahabad" , 9876543439 , null , 1 ),
+( 121, "Arya Pandey", 21 , "Allahabad" , 9876543441 , 9876543442 , 1 ),
+( 122, "Astha Patel", 22 , "Allahabad" , 9876543443 , 9876543444 , 1 ),
+( 123, "Atharva Rao", 23 , "Udaipur" , 9876543445 , 9876543446 , 1 ),
+( 124, "Avantika Roy", 24 , "Patna" , 9876543447 , null , 1 ),
+( 125, "Ayush Sharma", 25 , "Patna" , 9876543449 , null , 1 ),
+( 126, "Bhavya Singh", 26 , "Patna" , 9876543451 , 9876543452 , 1 ),
+( 127, "Bhumi Thakur", 27 , "Allahabad" , 9876543453 , 9876543454 , 1 ),
+( 128, "Chaitanya Verma", 28 , "Aligarh" , 9876543455 , 9876543456 , 1 ),
+( 129, "Chandni Yadav", 29 , "Patna" , 9876543457 , 9876543458 , 1 ),
+( 130, "Charu Joshi", 30 , "Udaipur" , 9876543459 , 9876543460 , 1 ),
+( 131, "Devansh Kumar", 31 , "Chandigarh" , 9876543461 , 9876543462 , 1 ),
+( 132, "Diya Mishra", 32 , "Chandigarh" , 9876543463 , 9876543464 , 1 ),
+( 133, "Dhruv Nair", 33 , "Patna" , 9876543465 , 9876543466 , 1 ),
+( 134, "Eesha Oberoi", 34 , "Chandigarh" , 9876543467 , 9876543468 , 1 ),
+( 135, "Fahmaan Pandey", 35 , "Aligarh" , 9876543469 , null , 1 ),
+( 136, "Fiza Patel", 36 , "Allahabad" , 9876543471 , 9876543472 , 1 ),
+( 137, "Gautam Rao", 37 , "Aligarh" , 9876543473 , 9876543474 , 1 ),
+( 138, "Gayatri Roy", 38 , "Delhi" , 9876543475 , 9876543476 , 1 ),
+( 139, "Gaurav Sharma", 39 , "Delhi" , 9876543477 , 9876543478 , 1 ),
+( 140, "Hansika Singh", 40 , "Gwalior" , 9876543479 , 9876543480 , 1 ),
+( 141, "Hardik Thakur", 41 , "Delhi" , 9876543481 , 9876543482 , 1 ),
+( 142, "Ishan Verma", 42 , "Gwalior" , 9876543483 , 9876543484 , 1 ),
+( 143, "Ishita Yadav", 43 , "Ajmer" , 9876543485 , 9876543486 , 1 ),
+( 144, "Ishaan Joshi", 44 , "Lucknow" , 9876543487 , null , 1 ),
+( 145, "Jeet Kumar", 45 , "Lucknow" , 9876543489 , 9876543490 , 1 ),
+( 146, "Jiya Mishra", 46 , "Allahabad" , 9876543491 , 9876543492 , 1 ),
+( 147, "Kabir Nair", 47 , "Varanasi" , 9876543493 , null , 1 ),
+( 148, "Kaveri Oberoi", 48 , "Lucknow" , 9876543495 ,null , 1 ),
+( 149, "Krish Pandey", 49 , "Meerut" , 9876543497 , 9876543498 , 1 ),
+( 150, "Manav Sharma", 50 , "Haridwar" , 9876543499 , 9876543500 , 1 ),
+( 151, "Neha Verma", 51 , "Lucknow" , 9876543501 , 9876543502 , 1 ),
+( 152, "Nisha Joshi", 52 , "Meerut" , 9876543503 , 9876543504 , 1 ),
+( 153, "Omkar Kumar", 53 , "Gwalior" , 9876543505 , 9876543506 , 1 ),
+( 154, "Priya Mishra", 54 , "Haridwar" , 9876543507 , 9876543508 , 1 ),
+( 155, "Vivaan Singh", 55 , "Shimla" , 9876543509 , 9876543510 , 1 ),
+( 156, "Yash Kumar", 56 , "Haridwar" , 9876543511 , 9876543512 , 1 ),
+( 157, "Yash Yadav", 57 , "Varanasi" , 9876543513 , null , 1 ),
+( 158, "Zara Dalal", 58 , "Varanasi" , 9876543515 , null , 1 ),
+( 159, "Zoya Verma", 59 , "Shimla" , 9876543517 , 9876543518 , 1 ),
+( 160, "Zoya khan", 60 , "Ajmer" , 9876543519 , 9876543520 , 1 );
+
+select * from BEd_student;
+
+truncate table BEd_student;
